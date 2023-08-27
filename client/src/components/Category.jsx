48fa374e218e0c6b14d38161f0e53485bccfe9f2
Nishanth <nisanthrtts@gmail.com>
2023-08-27 19:38:54 +0530

    UI improvements

--- client/src/components/Category.jsx
+++ client/src/components/Category.jsx
@@ -1,16 +1,26 @@
 const Category = ({ categories, selectedCategory, handleCategorySelect }) => {
   return (
     <div className='flex gap-6 my-6'>
-      <h1>Categories :</h1>
+      <h1 className='text-lg font-medium'>Categories :</h1>
       <ul className='flex gap-8'>
+        <li
+          className={`cursor-pointer ${
+            !selectedCategory
+              ? `font-medium bg-gray-800 text-white px-2 rounded-md`
+              : 'hover:underline'
+          }`}
+          onClick={() => handleCategorySelect(null)}
+        >
+          All
+        </li>
         {categories.map((category) => (
           <li
             key={category}
             onClick={() => handleCategorySelect(category)}
             className={`cursor-pointer ${
               category === selectedCategory
-                ? 'font-semibold bg-gray-800 text-white px-2 rounded-md'
-                : ''
+                ? `font-medium bg-gray-800 text-white px-2 rounded-md`
+                : 'hover:underline'
             }`}
           >
             {category}
